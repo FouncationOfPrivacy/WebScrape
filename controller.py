a788cbfe2470c963faefd63c057ae05b6a85049f
Derek Fan <haoping.fan@gmail.com>
2020-11-10 23:22:05 +0800

    unique url

--- controller.py
+++ controller.py
@@ -14,14 +14,14 @@ os.mkdir(sys.argv[3])
 
 
 # 5001_to_100000_sites.json
-with open(sys.argv[2], 'r') as hdle:
-	urls = json.load(hdle)
+# with open(sys.argv[2], 'r') as hdle:
+# 	urls = json.load(hdle)
 
-urls = random.choices(urls, k=5000)
+# urls = random.choices(urls, k=5000)
 
-for url in urls:
-	output_pathname = f'{sys.argv[3]}/{url}'
-	os.system(f'scrapy runspider control_crawl.py -a url={url} -a output_pathname={output_pathname} -a depth=3')
+# for url in urls:
+# 	output_pathname = f'{sys.argv[3]}/{url}'
+# 	os.system(f'scrapy runspider control_crawl.py -a url={url} -a output_pathname={output_pathname} -a depth=3')
 
 # top_5000_sites.json
 with open(sys.argv[1], 'r') as hdle:
@@ -32,10 +32,23 @@ for url in urls:
 	os.system(f'scrapy runspider control_crawl.py -a url={url} -a output_pathname={output_pathname} -a depth=3')
 
 # merge all url
-with open(f'{sys.argv[3]}/allUrl.txt', 'a') as allUrl:
+with open(f'{sys.argv[3]}/allUrl.txt', 'a+') as allUrl:
 	for subdir in os.listdir(sys.argv[3]):
 		path = f'{sys.argv[3]}/{subdir}/url.txt'
 
 		if os.path.exists(path):
 			with open(path, 'r') as eachUrl:
 				allUrl.write(eachUrl.read())
+
+# unique allUrl
+with open(f'{sys.argv[3]}/allUrl.txt', 'r') as allUrl:
+	urls = []
+
+	for line in allUrl:
+		urls.append(line)
+
+	urls = list(set(urls))
+
+with open(f'{sys.argv[3]}/allUniqueUrl.txt', 'a') as allUniqueUrl:
+	for url in urls:
+		allUniqueUrl.write(url)
