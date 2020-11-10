31f0942af7257a4f17d17a7afcf4f4225fb62e27
Derek Fan <haoping.fan@gmail.com>
2020-11-10 22:18:01 +0800

    add data directory in controller

--- controller.py
+++ controller.py
@@ -6,29 +6,36 @@ import json
 import shutil
 import random
 
-# 5001_to_100000_sites.json
-with open(sys.argv[2], 'r') as hdle:
-	urls = json.load(hdle)
+# add data directory
 
-urls = random.choices(urls, k=5000)
+if os.path.isdir(sys.argv[3]):
+	shutil.rmtree(sys.argv[3])
+os.mkdir(sys.argv[3])
 
-for url in urls:
-	output_pathname = f'{sys.argv[3]}/{url}'
-	os.system(f'scrapy runspider control_crawl.py -a url={url} -a output_pathname={output_pathname} -a depth=3')
 
-# top_5000_sites.json
-with open(sys.argv[1], 'r') as hdle:
-	urls = json.load(hdle)
+# # 5001_to_100000_sites.json
+# with open(sys.argv[2], 'r') as hdle:
+# 	urls = json.load(hdle)
 
-for url in urls:
-	output_pathname = f'{sys.argv[3]}/{url}'
-	os.system(f'scrapy runspider control_crawl.py -a url={url} -a output_pathname={output_pathname} -a depth=3')
+# urls = random.choices(urls, k=5000)
 
-# merge all url
-with open(f'{sys.argv[3]}/allUrl.txt', 'a') as allUrl:
-	for subdir in os.listdir(sys.argv[3]):
-		path = f'{sys.argv[3]}/{subdir}/url.txt'
+# for url in urls:
+# 	output_pathname = f'{sys.argv[3]}/{url}'
+# 	os.system(f'scrapy runspider control_crawl.py -a url={url} -a output_pathname={output_pathname} -a depth=3')
 
-		if os.path.exists(path):
-			with open(path, 'r') as eachUrl:
-				allUrl.write(eachUrl.read())
+# # top_5000_sites.json
+# with open(sys.argv[1], 'r') as hdle:
+# 	urls = json.load(hdle)
+
+# for url in urls:
+# 	output_pathname = f'{sys.argv[3]}/{url}'
+# 	os.system(f'scrapy runspider control_crawl.py -a url={url} -a output_pathname={output_pathname} -a depth=3')
+
+# # merge all url
+# with open(f'{sys.argv[3]}/allUrl.txt', 'a') as allUrl:
+# 	for subdir in os.listdir(sys.argv[3]):
+# 		path = f'{sys.argv[3]}/{subdir}/url.txt'
+
+# 		if os.path.exists(path):
+# 			with open(path, 'r') as eachUrl:
+# 				allUrl.write(eachUrl.read())
